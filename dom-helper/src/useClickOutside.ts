b54d59f5a2ee229ec929d4abad277745b3ee20f7
汪洋 <wangyang2010344@foxmail.com>
2022-07-25 22:05:00 +0800

    add

---
+++ dom-helper/src/useClickOutside.ts
@@ -0,0 +1,19 @@
+import { useEffect } from "better-react"
+import { useEvent } from 'better-react-helper'
+
+export default (
+  contains: (e: Node) => boolean,
+  click: (e: MouseEvent) => void
+) => {
+  const onClick = useEvent((e: MouseEvent) => {
+    if (!contains(e.target as Node)) {
+      click(e)
+    }
+  })
+  useEffect(() => {
+    document.addEventListener("click", onClick)
+    return function () {
+      document.removeEventListener("click", onClick)
+    }
+  }, [])
+}
